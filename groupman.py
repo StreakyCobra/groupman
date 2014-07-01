6c57007daae09d750ce424aac17b5e079467fb7b
Fabien Dubosson <fabien.dubosson@gmail.com>
2014-07-01 14:10:40 +0200

    Remove useless docstring in main script

--- groupman.py
+++ groupman.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Gather your Arch Linux packages in groups to simplify their management.
-"""
 
 from groupman.__main__ import run
 
 
 def main():
     run()
+
 
 # When used as script
 if __name__ == "__main__":
