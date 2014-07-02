96a6d7351e0e362739c37d07d8e5db2a214aa122
Fabien Dubosson <fabien.dubosson@gmail.com>
2014-07-02 14:34:28 +0200

    Small corrections

--- groupman/commands/packages.py
+++ groupman/commands/packages.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 """Give the status of the packages."""
 
-from groupman.core.config import g
 from groupman.core.db import db_list
-from groupman.core.groups import group_info, installed_packages
-from groupman.core.pacman import pacman
+from groupman.core.groups import group_info, installed_packages, deps_packages
 from groupman.core.prettyprint import pr_list, pr_info
 
 _name = 'packages'
@@ -21,7 +19,7 @@ def run(args):
     # Get installed groups from DB
     groups = map(group_info, db_list())
     # List of needed packages
-    desired = [p for group in groups for p in group['packages']]
+    desired = [d for group in groups for d in deps_packages(group['name'])]
     # List of installed packages
     installed = installed_packages()
     # List of packages to install
