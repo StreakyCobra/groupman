368b79c5bd7c5dd9b33dcb0829e1b4322e89ad78
Fabien Dubosson <fabien.dubosson@gmail.com>
2014-07-09 11:34:20 +0200

    Update docstring

--- groupman/extra/packages.py
+++ groupman/extra/packages.py
@@ -6,29 +6,31 @@ from groupman.core.pacman import pacman
 from groupman.extra.groups import installed_groups
 
 
-def remove_unmanaged(packages):
+def _remove_unmanaged(packages):
+    """Remove unmanaged ."""
     # Get unmanaged packages
     unmanaged = pacman(['-Qgq'] + get('IGNORE_GROUPS', aslist=True),
                        False).strip().split('\n')
     # Remove unmanaged packages
     filtered = [x for x in packages if x not in unmanaged]
+    # Return the filtered list of packages
     return sorted(list(set(filtered)))
 
 
 def all_installed_packages():
-    """List explicitly installed packages that are not in base or base-devel."""
-    # Get all explicitly installed packages
-    explicit_packages = pacman(['-Qq'], False).strip().split('\n')
-    # Return all explicitly install package without unmanaged ones
-    return remove_unmanaged(explicit_packages)
+    """List all installed packages."""
+    # Get all installed packages
+    all_packages = pacman(['-Qq'], False).strip().split('\n')
+    # Return all installed packages without unmanaged ones
+    return _remove_unmanaged(all_packages)
 
 
 def explict_installed_packages():
-    """List explicitly installed packages that are not in base or base-devel."""
+    """List explicitly installed packages without unmanaged ones."""
     # Get all explicitly installed packages
     explicit_packages = pacman(['-Qeq'], False).strip().split('\n')
-    # Return all explicitly install package without unmanaged ones
-    return remove_unmanaged(explicit_packages)
+    # Return all explicitly installed packages without unmanaged ones
+    return _remove_unmanaged(explicit_packages)
 
 
 def desired_packages():
@@ -41,7 +43,5 @@ def desired_packages():
     unwanted = [p for group in groups for p in group['all_removed']]
     # Remove unwanted packages
     desired = [p for p in desired if p not in unwanted]
-    # Remove unmanaged packages
-    desired = remove_unmanaged(desired)
-    # Return desired packages
-    return desired
+    # Return desired packages without unmanaged ones
+    return _remove_unmanaged(desired)
